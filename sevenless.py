26f85e259db515cc38e8cf792d9f02c4340622ed
Sun Hyun Chang <schan131@pelican.ib.hpcc.ucr.edu>
2019-10-25 16:15:29 -0700

    homework updated

--- sevenless.py
+++ sevenless.py
@@ -4,3 +4,13 @@ start = 0
 end   = 99
 divisor=7
 print("Printing out numbers from",start,"to",end, " not divisible by",divisor)
+
+i = 0
+while (i < 99):
+	i = i +1
+	if i % 7 != 0:
+		print(i)
+	
+
+
+
