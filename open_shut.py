5fba352120ef9b9feb9caafc77c60c61e36ab122
Sun Hyun Chang <schan131@pigeon.ib.hpcc.ucr.edu>
2019-10-28 16:58:49 -0700

    2nd question

--- open_shut.py
+++ open_shut.py
@@ -1,24 +1,40 @@
 #!/usr/bin/env python
-
-import os,csv,gzip
+import os,csv,gzip,re,sys
 
 # write a script to create new file called closed.txt
 
-outfile="closed.txt"
-# on the HPCC cluster you can use the file
-file="/bigdata/gen220/shared/simple/title.basics.tsv.gz"
-# or if you run this on your own computer use this
-#file="title.basics.tsv.gz"
+#if not os.path.exists(file):
+#    os.system("curl -O https://datasets.imdbws.com/title.akas.tsv.gz")
 
+ #create output file and add contents
+sys.stdout = open("closed.txt", "w")
 
-if not os.path.exists(file):
-    os.system("curl -O https://datasets.imdbws.com/title.akas.tsv.gz")
-
+#title.basics.tsv.gz in bigdata/gen220/shared/simple folder seems disrupted -> use a file in my folder.
 with gzip.open("/rhome/schan131/2019_hw2_schan131/title.basics.tsv.gz", "rt", encoding="utf-8") as file:
-   reader = csv.reader(file, delimiter="\t")
-   rownum = 0
-   for row in reader:
-      location = row[3]
-      if location.lower().find("door") != -1:
-         rownum = rownum + 1
-   print("Answer 1 is", rownum)
+  reader = csv.reader(file, delimiter="\t") #open tsv file
+
+  everydoor = [] #Q1 
+  Onlydoor = [] #Q2
+  withOpen = [] #Q3a
+  withClosed = [] #Q3b
+
+  for row in reader:
+    location = row[3] #only use titles in column 4 to avoid count the same things multiple times
+    Option1 = re.search(r"[dD]oor", location)
+    Option2 = re.search(r"(^|\s)[dD]oor(\s|$)", location)
+    Option3a = re.search(r"(^|\s)Open(\s|$)", location)
+    Option3b = re.search(r"(^|\s)Closed(\s|$)", location)
+    if Option1:
+      everydoor.append(location)
+    if Option2:
+      Onlydoor.append(location)
+    if Option3a:
+      withOpen.append(location)
+    if Option3b:
+      withClosed.append(location)
+      
+  print("Answer 1 is", len(everydoor), "\n", 
+ "Answer 2 is", len(Onlydoor), "\n", 
+ "Answer 3a: the number of movies with 'Open' is", len(withOpen), "\n", 
+ "Answer 3b: the number of movies with 'Closed' is", len(withOpen), "\n")    
+
